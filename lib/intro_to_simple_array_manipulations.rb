7e947c2b50e0a3e1556b1e29d9154f77d453422b
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-24 03:10:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+def using_push(array, string)
+  array.push(string)
+
+end
+ 
+def using_unshift(array, string)
+  array.unshift(string)
+end
+  
+def using_pop(arr)
+  arr.pop
+end 
+
+def pop_with_args(arr)
+  arr.pop(2)
+end 
+
+def using_shift(arr)
+ arr.shift
+end
+
+def shift_with_args
+ arr.shift(2)
+end
+
+def using_concat
+
+end
+
+def using_insert
+
+end
+
+def using_uniq
+
+end
+
+def using_flatten
+
+end
+
+def using_delete
+
+end
+
+def using_delete_at
+
+end
+           
